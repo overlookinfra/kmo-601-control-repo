4399f52797363326ccf2fbc6a29155bdf07c2f2e
Alex Williamson <89106273+awilliamson-puppet@users.noreply.github.com>
2022-06-07 09:46:39 -0400

    Create new role for lab
    Create new role for lab

---
+++ site-modules/role/manifests/appserver.pp
@@ -0,0 +1,12 @@
+
+   
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include role::appserver
+class role::appserver {
+  include profile::base
+  include nginx
+}
